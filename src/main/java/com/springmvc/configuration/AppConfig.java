5037eae48d3d78c9f436675f7b3c3c274457f754
Pritiranjan <pritiranjan.c@gmail.com>
2019-12-18 14:01:37 +0530

    This is second commit

--- src/main/java/com/springmvc/configuration/AppConfig.java
+++ src/main/java/com/springmvc/configuration/AppConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "com.spring")
+@ComponentScan(basePackages = "com.springmvc")
 public class AppConfig {
 	
 	@Bean
